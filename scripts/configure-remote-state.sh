f02b57e69061ed9080dfe39240657645dadf4943
Le Anh Dao <daodidem@gmail.com>
2017-09-19 14:34:09 +0700

    ABBBB

--- scripts/configure-remote-state.sh
+++ scripts/configure-remote-state.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sudo -s
 terraform remote config -backend=s3 -backend-config="bucket=terraform-state-daoanhle2" -backend-config="key=terraform/terraform.tfstate" -backend-config="region=us-west-2"
