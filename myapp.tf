7048116b5c6a8857096f53678bc42efc7cda00ff
Le Anh Dao <daodidem@gmail.com>
2017-09-24 13:40:16 +0700

    Edit load balancer

--- myapp.tf
+++ myapp.tf
@@ -23,7 +23,8 @@ resource "aws_ecs_service" "myapp-service" {
   depends_on = ["aws_iam_policy_attachment.ecs-service-attach1"]
 
   load_balancer {
-    elb_name = "${aws_elb.myapp-elb.name}"
+    elb_name = "myapp-elb-2"
+	#elb_name = "${aws_elb.myapp-elb.name}"
     container_name = "myapp"
     container_port = 3000
   }
@@ -31,35 +32,7 @@ resource "aws_ecs_service" "myapp-service" {
 }
 
 # load balancer
-resource "aws_elb" "myapp-elb" {
-  name = "myapp-elb"
-  listener {
-    instance_port = 3000
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
-  }
 
-  health_check {
-    healthy_threshold = 3
-    unhealthy_threshold = 3
-    timeout = 30
-    target = "HTTP:3000/"
-    interval = 60
-  }
-
-  cross_zone_load_balancing = true
-  idle_timeout = 400
-  connection_draining = true
-  connection_draining_timeout = 400
-
-  subnets = ["${aws_subnet.main-public-1.id}","${aws_subnet.main-public-2.id}"]
-  security_groups = ["${aws_security_group.myapp-elb-securitygroup.id}"]
-
-  tags {
-    Name = "myapp-elb-myapptf"
-  }
-}
 
 
 
